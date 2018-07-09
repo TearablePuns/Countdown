1acf3e771895edb50d7f5dca82226e2f403f8e55
TearablePuns <jmemmott13@gmail.com>
2018-07-09 09:44:16 -0600

    Added functionality for dates in the past

--- src/components/app.js
+++ src/components/app.js
@@ -37,11 +37,32 @@ export default class App extends Component {
   handleGenerate = function () {
     this.setState({ active: true })
 
-    var countDownDate = this.state.startDate.toDate().getTime();
+    var bday = this.state.startDate.toDate();
+    var today = new Date();
+    var currentMonth = today.getMonth();
+    var birthMonth = bday.getMonth();
+
+    if(birthMonth > currentMonth) {
+      bday.setFullYear(today.getFullYear())
+    } else if(birthMonth < currentMonth) {
+      bday.setFullYear(today.getFullYear() + 1)
+    } else if(birthMonth == currentMonth) {
+      var currentDay = today.getDate();
+      var birthDay = bday.getDate();
+
+      if(birthDay > currentDay) {
+        bday.setFullYear(today.getFullYear())
+      }
+      if(birthDay <= currentDay) {
+        bday.setFullYear(today.getFullYear() + 1)
+      }
+    }
+
+    var countDownDate = bday.getTime();
 
     this.timer = setInterval(function () {
 
-      var now = new Date().getTime();
+      var now = today.getTime();
       var distance = countDownDate - now;
 
       var days = Math.floor(distance / (1000 * 60 * 60 * 24));
@@ -69,7 +90,7 @@ export default class App extends Component {
   renderItems = function () {
     if (this.state.active) {
       return [
-        <Clock timeRemaining={this.state.timeRemaining}/>,
+        <Clock timeRemaining={this.state.timeRemaining} />,
         ChangeDate('Change Date', () => this.setState({ active: false })),
         LargeText('04/03'),
         <label className="grid__remaining">remaining until the next occurrence of this event</label>
