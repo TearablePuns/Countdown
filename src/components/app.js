b75e89f49c3cc25e2122169c33ffe85d3580b33d
TearablePuns <jmemmott13@gmail.com>
2018-07-09 10:23:08 -0600

    Added age calculation and FINALLY figured out how to add number endings like 'st' 'nd' 'rd' and 'th'

--- src/components/app.js
+++ src/components/app.js
@@ -20,7 +20,8 @@ export default class App extends Component {
         hours: 0,
         minutes: 0,
         seconds: 0
-      }
+      },
+      age: 0
     }
 
     this.handleGenerate = this.handleGenerate.bind(this);
@@ -35,25 +36,52 @@ export default class App extends Component {
   }.bind(this)
 
   handleGenerate = function () {
-    this.setState({ active: true })
 
     var bday = this.state.startDate.toDate();
     var today = new Date();
     var currentMonth = today.getMonth();
     var birthMonth = bday.getMonth();
 
-    if(birthMonth > currentMonth) {
+
+    var timeBetween = today.getTime() - bday.getTime();
+    var daysOld = Math.floor(timeBetween / (1000 * 60 * 60 * 24))
+    var yearsOld = Number((daysOld / 365).toFixed(0));
+    var ageString = yearsOld.toString();
+    this.setState({
+      age: yearsOld,
+      active: true,
+      affix: "th"
+    })
+    if ((ageString.charAt(ageString.length - 1) == 1) && ageString.charAt(ageString.length - 2) != 1) {
+      this.setState({
+        affix: "st"
+      })
+    } else if ((ageString.charAt(ageString.length - 1) == 2) && ageString.charAt(ageString.length - 2) != 1) {
+      this.setState({
+        affix: "nd"
+      })
+    } else if ((ageString.charAt(ageString.length - 1) == 3) && ageString.charAt(ageString.length - 2) != 1) {
+      this.setState({
+        affix: "rd"
+      })
+    } else {
+      this.setState({
+        affix: "th"
+      })
+    }
+    console.log(ageString);
+    if (birthMonth > currentMonth) {
       bday.setFullYear(today.getFullYear())
-    } else if(birthMonth < currentMonth) {
+    } else if (birthMonth < currentMonth) {
       bday.setFullYear(today.getFullYear() + 1)
-    } else if(birthMonth == currentMonth) {
+    } else if (birthMonth == currentMonth) {
       var currentDay = today.getDate();
       var birthDay = bday.getDate();
 
-      if(birthDay > currentDay) {
+      if (birthDay > currentDay) {
         bday.setFullYear(today.getFullYear())
       }
-      if(birthDay <= currentDay) {
+      if (birthDay <= currentDay) {
         bday.setFullYear(today.getFullYear() + 1)
       }
     }
@@ -79,7 +107,7 @@ export default class App extends Component {
       }
 
       this.setState({ timeRemaining })
-      console.log(this.state.timeRemaining)
+      // console.log(this.state.timeRemaining)
 
       if (distance < 0) {
         clearInterval(this.timer);
@@ -93,7 +121,7 @@ export default class App extends Component {
         <Clock timeRemaining={this.state.timeRemaining} />,
         ChangeDate('Change Date', () => this.setState({ active: false })),
         LargeText('04/03'),
-        <label className="grid__remaining">remaining until the next occurrence of this event</label>
+        <label className="grid__remaining">remaining until the {this.state.age}{this.state.affix} occurrence of this event</label>
       ]
     } else {
       return [
